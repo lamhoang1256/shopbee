555df03cf801a5220c7f42ad684e59c12cf5292c
lamhoang1256 <lamhoang1256@gmail.com>
2022-08-16 21:34:49 +0700

    Style: change style UI ProductManage to table

--- src/modules/product/ProductManage.tsx
+++ src/modules/product/ProductManage.tsx
@@ -1,27 +1,31 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+import { IPagination, IProduct } from "@types";
 import { productAPI } from "apis";
-import { ActionDelete } from "components/action";
+import { Button } from "components/button";
 import { Loading } from "components/loading";
+import { Pagination } from "components/pagination";
 import { path } from "constants/path";
-import { IProduct } from "@types";
 import { HeaderTemplate } from "layouts";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import { formatCash, formatMoney } from "utils/helper";
+import { formatMoney } from "utils/helper";
 import ProductImage from "./ProductImage";
 import ProductPriceSale from "./ProductPriceSale";
-import ProductRating from "./ProductRating";
-import ProductTitle from "./ProductTitle";
 
 const ProductManage = () => {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [loading, setLoading] = useState(true);
+  const [pagination, setPagination] = useState<IPagination>(Object);
+  const [searchParams] = useSearchParams();
+  const params = Object.fromEntries(searchParams);
+
   const fetchAllProduct = async () => {
     setLoading(true);
     try {
-      const { data } = await productAPI.getAllProduct();
+      const { data } = await productAPI.getAllProduct({ ...params, limit: "10" });
       setProducts(data?.products);
+      setPagination(data.pagination);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -42,7 +46,7 @@ const ProductManage = () => {
 
   useEffect(() => {
     fetchAllProduct();
-  }, []);
+  }, [searchParams]);
   return (
     <HeaderTemplate
       label='Quản lí sản phẩm'
@@ -50,29 +54,53 @@ const ProductManage = () => {
     >
       {loading && <Loading />}
       {!loading && (
-        <div className='gap-3 my-3 product-grid'>
-          {products?.map((product) => {
-            const { price, image, _id, name, rating, sold } = product;
-            return (
-              <Link
-                to={`${path.productUpdate}/${_id}`}
-                className='relative bg-white shadow-product'
-                key={_id}
-              >
-                <ProductImage imageUrl={image} />
-                <div className='p-2 pb-4'>
-                  <ProductTitle>{name}</ProductTitle>
-                  <div className='flex flex-col my-1 gap-x-2 gap-y-1 md:items-center md:flex-row'>
-                    <ProductRating rating={rating} size='w-[14px] h-[14px]' />
-                    <span className='text-[#787878] text-xs'>Đã bán {formatCash(sold)}</span>
-                  </div>
-                  <ProductPriceSale>{formatMoney(price)}</ProductPriceSale>
-                </div>
-                <ActionDelete onClick={() => handleDeleteProduct(_id)} className='!w-5 !h-5' />
-              </Link>
-            );
-          })}
-        </div>
+        <>
+          <div className='tables'>
+            <table>
+              <thead className=''>
+                <tr>
+                  <th>STT</th>
+                  <th>Tên sản phẩm</th>
+                  <th>Hình ảnh</th>
+                  <th>Số lượng</th>
+                  <th>Giá gốc</th>
+                  <th>Giá khuyến mãi</th>
+                  <th>Hành động</th>
+                </tr>
+              </thead>
+              <tbody>
+                {products?.map((product, index) => (
+                  <tr key={product._id}>
+                    <td>{index + 1}</td>
+                    <td className='w-[300px]'>
+                      <p className='!whitespace-pre-line line-clamp-2'>{product.name}</p>
+                    </td>
+                    <td>
+                      <ProductImage
+                        imageUrl={product.image}
+                        className='border rounded w-14 h-14 border-slate-200'
+                      />
+                    </td>
+                    <td>{product.stock}</td>
+                    <td>{formatMoney(product.oldPrice)}</td>
+                    <td>
+                      <ProductPriceSale className='text-sm'>
+                        {formatMoney(product.price)}
+                      </ProductPriceSale>
+                    </td>
+                    <td>
+                      <div className='flex gap-x-1'>
+                        <Button to={`${path.productUpdate}/${product._id}`}>Sửa</Button>
+                        <Button onClick={() => handleDeleteProduct(product._id)}>Xóa</Button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          {products.length > 0 && <Pagination pagination={pagination} />}
+        </>
       )}
     </HeaderTemplate>
   );
