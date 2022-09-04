3e2fc906cc3ad2d6186673ac9078f13582c3c099
lamhoang1256 <lamhoang1256@gmail.com>
2022-09-04 11:11:02 +0700

    Feat: handle change order status by admin

--- src/apis/orderAPI.ts
+++ src/apis/orderAPI.ts
@@ -14,6 +14,18 @@ export const orderAPI = {
     const path = `api/order/${id}`;
     return axiosClient.get(path);
   },
+  processingOrder: (id: string): Promise<IResponse> => {
+    const path = `api/order/${id}/processing`;
+    return axiosClient.put(path);
+  },
+  shippingOrder: (id: string): Promise<IResponse> => {
+    const path = `api/order/${id}/shipping`;
+    return axiosClient.put(path);
+  },
+  deliveredOrder: (id: string): Promise<IResponse> => {
+    const path = `api/order/${id}/delivered`;
+    return axiosClient.put(path);
+  },
   cancelOrder: (id: string, payload: IPayloadCancelOrder): Promise<IResponse> => {
     const path = `api/order/${id}/canceled`;
     return axiosClient.put(path, payload);
--- src/modules/order/OrderUpdate.tsx
+++ src/modules/order/OrderUpdate.tsx
@@ -1,10 +1,9 @@
-import { IOrder, OrderStatusVietnamese } from "@types";
+import { IOrder, OrderStatus, OrderStatusVietnamese } from "@types";
 import { orderAPI } from "apis";
-import { Button } from "components/button";
 import { SectionWhite } from "components/common";
 import { Loading } from "components/loading";
 import { Option, Select } from "components/select";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import OrderHeader from "./OrderHeader";
@@ -14,7 +13,7 @@ import OrderProduct from "./OrderProduct";
 import OrderProgress from "./OrderProgress";
 
 const OrderUpdate = () => {
-  const { id } = useParams();
+  const { id = "" } = useParams();
   const [loading, setLoading] = useState(true);
   const [order, setOrder] = useState<IOrder>(Object);
   const fetchDetailsOrder = async () => {
@@ -28,6 +27,37 @@ const OrderUpdate = () => {
       setLoading(false);
     }
   };
+
+  const requestUpdateStatusOrder = async (status: string) => {
+    if (status === OrderStatus.processing) {
+      const { message } = await orderAPI.processingOrder(id);
+      toast.success(message);
+    }
+    if (status === OrderStatus.shipping) {
+      const { message } = await orderAPI.shippingOrder(id);
+      toast.success(message);
+    }
+    if (status === OrderStatus.delivered) {
+      const { message } = await orderAPI.deliveredOrder(id);
+      toast.success(message);
+    }
+    if (status === OrderStatus.canceled) {
+      const payload = { reasonCancel: "Đơn hàng được hủy bởi Shopbee" };
+      const { message } = await orderAPI.cancelOrder(id, payload);
+      toast.success(message);
+    }
+  };
+
+  const handleUpdateStatusOrder = async (e: ChangeEvent<HTMLSelectElement>) => {
+    try {
+      const status = e.target.value;
+      await requestUpdateStatusOrder(status);
+      fetchDetailsOrder();
+    } catch (error) {
+      toast.error(error?.message);
+    }
+  };
+
   useEffect(() => {
     fetchDetailsOrder();
   }, [id]);
@@ -43,13 +73,13 @@ const OrderUpdate = () => {
       <SectionWhite className='mt-3'>
         <div className='flex items-center gap-x-5'>
           <span>Trạng thái đơn hàng</span>
-          <Select name='status'>
-            <Option value='1'>Chờ xác nhận</Option>
-            <Option value='2'>Đã thanh toán</Option>
-            <Option value='3'>Đang vận chuyển</Option>
-            <Option value='4'>Đã giao hàng</Option>
+          <Select name='status' onChange={handleUpdateStatusOrder}>
+            <Option value=''>Trạng thái</Option>
+            <Option value={OrderStatus.processing}>Đang xử lí</Option>
+            <Option value={OrderStatus.shipping}>Đang vận chuyển</Option>
+            <Option value={OrderStatus.delivered}>Đã giao hàng</Option>
+            <Option value={OrderStatus.canceled}>Hủy</Option>
           </Select>
-          <Button primary>Chỉnh sửa trạng thái</Button>
         </div>
       </SectionWhite>
       <SectionWhite className='mt-3'>
