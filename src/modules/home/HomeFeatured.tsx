712d3e1725bb38098e21de2e71d87c3173d6f6ea
lamhoang1256 <lamhoang1256@gmail.com>
2022-09-05 18:37:02 +0700

    Chore: handle scrollIntoView HomeFeatured

--- src/modules/home/HomeFeatured.tsx
+++ src/modules/home/HomeFeatured.tsx
@@ -2,11 +2,16 @@ import { SectionHeader } from "components/common";
 import { Pagination } from "components/pagination";
 import useFetchProducts from "hooks/useFetchProducts";
 import { ProductGrid } from "modules/product";
+import { useEffect, useRef } from "react";
 
 const HomeFeatured = () => {
-  const { products, pagination } = useFetchProducts();
+  const { products, searchParams, pagination } = useFetchProducts();
+  const sectionRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (sectionRef.current) sectionRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [searchParams]);
   return (
-    <div className='layout-container'>
+    <div className='layout-container' ref={sectionRef}>
       <SectionHeader>
         <h3 className='text-base font-medium text-orangeee4'>GỢI Ý HÔM NAY</h3>
       </SectionHeader>
